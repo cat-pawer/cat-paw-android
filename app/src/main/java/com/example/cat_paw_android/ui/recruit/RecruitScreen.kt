a71c7e0ca22780778512effa9f9e21da2500467e
ChoiUS <chldn323@gmail.com>
2023-11-18 14:13:12 +0900

    feat: 팀원 구인 프로젝트 카드 수정

--- app/src/main/java/com/example/cat_paw_android/ui/recruit/RecruitScreen.kt
+++ app/src/main/java/com/example/cat_paw_android/ui/recruit/RecruitScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -180,8 +181,8 @@ fun ProjectCard(project: Project) {
         .shadow(8.dp),
         onClick = {}) {
         Column(
-            modifier = Modifier.padding(10.dp),
-            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.padding(10.dp).fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceAround,
         ) {
             Row {
                 for (tag in project.tags) {
