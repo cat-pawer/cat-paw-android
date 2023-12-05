1bd56b02d1d22e1f69707c6622aa51ad12fa53f2
ChoiUS <chldn323@gmail.com>
2023-12-05 15:43:53 +0900

    feat: 타입 enum 추가

---
+++ domain/recruit/src/main/java/com/catpaw/recruit/model/OnlineType.kt
@@ -0,0 +1,7 @@
+package com.catpaw.recruit.model
+
+enum class OnlineType(
+    val korean: String,
+) {
+    ONLINE("온라인"), OFFLINE("오프라인"), COMPOSITE("온오프라인 병행")
+}
---
+++ domain/recruit/src/main/java/com/catpaw/recruit/model/RecruitType.kt
@@ -0,0 +1,7 @@
+package com.catpaw.recruit.model
+
+enum class RecruitType(
+    val korean: String
+) {
+    PROJECT("프로젝트"), STUDY("스터디")
+}
