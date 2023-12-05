21bfcfd564b701027a49a628bbb2730c5c9d7390
ChoiUS <chldn323@gmail.com>
2023-12-06 01:02:23 +0900

    feat: RecruitDetailUiState에 댓글 목록 추가

--- ui/src/main/java/com/catpaw/ui/recruitdetail/RecruitDetailUiState.kt
+++ ui/src/main/java/com/catpaw/ui/recruitdetail/RecruitDetailUiState.kt
@@ -1,7 +1,9 @@
 package com.catpaw.ui.recruitdetail
 
+import com.catpaw.recruit.model.RecruitComment
 import com.catpaw.recruit.model.RecruitDetail
 
 data class RecruitDetailUiState(
-    val recruitDetail: RecruitDetail = RecruitDetail()
+    val recruitDetail: RecruitDetail = RecruitDetail(),
+    val comment: List<RecruitComment> = listOf()
 )
