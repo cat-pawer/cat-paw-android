b7fddd82c6975d6d8096779249b64342d2084334
ChoiUS <chldn323@gmail.com>
2023-12-10 00:12:56 +0900

    feat: 댓글 가져오는 Retrofit Service 추가

--- data/recruit/src/main/java/com/catpaw/recruit/datasource/remote/service/DetailService.kt
+++ data/recruit/src/main/java/com/catpaw/recruit/datasource/remote/service/DetailService.kt
@@ -1,12 +1,21 @@
 package com.catpaw.recruit.datasource.remote.service
 
 import com.catpaw.recruit.dto.CatPawResponse
+import com.catpaw.recruit.dto.CommentListRequest
+import com.catpaw.recruit.dto.CommentListResponse
 import com.catpaw.recruit.dto.RecruitDetailResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.QueryMap
 
 interface DetailService {
     @GET("recruit/detail/{id}")
     suspend fun fetchRecruitDetail(@Path("id") id: Int): Response<CatPawResponse<RecruitDetailResponse>>
+
+    @GET("comment/summary/{id}")
+    suspend fun fetchDetailComment(
+        @Path("id") id: Int,
+        @QueryMap request: CommentListRequest,
+    ): Response<CatPawResponse<CommentListResponse>>
 }
