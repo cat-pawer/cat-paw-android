68f6041d494c47815f91aa48cb1c83eb9e9a1437
ChoiUS <chldn323@gmail.com>
2023-12-10 00:11:30 +0900

    feat: 댓글 가져오는 DataSource 구현

--- data/recruit/src/main/java/com/catpaw/recruit/datasource/remote/DetailDataSource.kt
+++ data/recruit/src/main/java/com/catpaw/recruit/datasource/remote/DetailDataSource.kt
@@ -1,9 +1,12 @@
 package com.catpaw.recruit.datasource.remote
 
 import com.catpaw.recruit.dto.CatPawResponse
+import com.catpaw.recruit.dto.CommentListResponse
 import com.catpaw.recruit.dto.RecruitDetailResponse
 import kotlinx.coroutines.flow.Flow
 
 interface DetailDataSource {
     fun getDetail(id: Int): Flow<Result<CatPawResponse<RecruitDetailResponse>>>
+
+    fun getCommentList(id: Int): Flow<Result<CatPawResponse<CommentListResponse>>>
 }
--- data/recruit/src/main/java/com/catpaw/recruit/datasource/remote/DetailDataSourceImpl.kt
+++ data/recruit/src/main/java/com/catpaw/recruit/datasource/remote/DetailDataSourceImpl.kt
@@ -2,7 +2,10 @@ package com.catpaw.recruit.datasource.remote
 
 import com.catpaw.recruit.datasource.remote.service.DetailService
 import com.catpaw.recruit.dto.CatPawResponse
+import com.catpaw.recruit.dto.CommentListRequest
+import com.catpaw.recruit.dto.CommentListResponse
 import com.catpaw.recruit.dto.RecruitDetailResponse
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
@@ -11,6 +14,20 @@ class DetailDataSourceImpl @Inject constructor(
 ) : DetailDataSource {
     override fun getDetail(id: Int) = flow<Result<CatPawResponse<RecruitDetailResponse>>> {
         val response = detailService.fetchRecruitDetail(id)
+        if (response.isSuccessful) {
+            val body = response.body()
+            if (body == null) {
+                emit(Result.failure(Exception("body is null")))
+            } else {
+                emit(Result.success(body))
+            }
+        } else {
+            emit(Result.failure(Exception(response.errorBody()?.string())))
+        }
+    }
+
+    override fun getCommentList(id: Int): Flow<Result<CatPawResponse<CommentListResponse>>> = flow {
+        val response = detailService.fetchDetailComment(id, CommentListRequest())
         if (response.isSuccessful) {
             val body = response.body()
             if (body == null) {
