7fbf6f49f326a71f18524b1a82d9268cbbe0de94
ChoiUS <chldn323@gmail.com>
2023-12-22 14:15:14 +0900

    feat: 프로젝트 목록 UiState 수정

--- ui/src/main/java/com/catpaw/ui/recruit/RecruitUiState.kt
+++ ui/src/main/java/com/catpaw/ui/recruit/RecruitUiState.kt
@@ -1,5 +1,10 @@
 package com.catpaw.ui.recruit
 
+import com.catpaw.recruit.model.Project
+
 data class RecruitUiState(
     val searchKeyword: String = "",
+    val newestRecruitList: List<Project> = listOf(),
+    val oldestRecruitList: List<Project> = listOf(),
+    val allRecruitList: List<Project> = listOf(),
 )
