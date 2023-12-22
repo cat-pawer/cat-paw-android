6e8b3c176ccb60cebb8274ee434c8558b8bb4c9b
ChoiUS <chldn323@gmail.com>
2023-12-22 20:26:03 +0900

    feat: Recruit Summary java.util.Date -> java.time.LocalDate 수정

--- data/recruit/src/main/java/com/catpaw/recruit/datasource/remote/SummaryDataSource.kt
+++ data/recruit/src/main/java/com/catpaw/recruit/datasource/remote/SummaryDataSource.kt
@@ -7,13 +7,14 @@ import com.catpaw.recruit.model.RecruitState
 import com.catpaw.recruit.model.RecruitType
 import com.catpaw.recruit.model.SearchTopic
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 import java.util.Date
 
 interface SummaryDataSource {
     fun getRecruitListByTopics(
         topic: SearchTopic,
         state: RecruitState,
-        period: Date,
+        period: LocalDate,
         page: Int = 0,
         size: Int = 10,
         vararg sort: String = arrayOf("created"),
@@ -25,7 +26,7 @@ interface SummaryDataSource {
         onlineType: OnlineType,
         categoryIdList: Array<Int>,
         state: RecruitState,
-        period: Date,
+        period: LocalDate,
         page: Int = 0,
         size: Int = 10,
         vararg sort: String = arrayOf("created"),
