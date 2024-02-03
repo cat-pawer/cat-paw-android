d7fc6ead4f7322ec47111f91e7f84001e4d8cb3a
ChoiUS <chldn323@gmail.com>
2024-02-04 00:00:18 +0900

    build: compose navigation, icons 추가

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -52,14 +52,20 @@ dependencies {
     kapt("com.google.dagger:hilt-compiler:$hiltVersion")
 
     // UI
+    val composeVersion = "1.6.0"
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-    implementation("androidx.compose.ui:ui:1.5.4")
-    implementation("androidx.compose.ui:ui-graphics:1.5.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-graphics:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation ("androidx.compose.material:material-icons-extended:$composeVersion")
+
+    val navVersion = "2.7.6"
+    implementation("androidx.navigation:navigation-compose:$navVersion")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
